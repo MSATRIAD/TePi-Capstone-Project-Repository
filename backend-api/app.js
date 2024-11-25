e911fca963fa197b1c20337f6988d20b4dab0321
IvanArsy <ivan22003@mail.unpad.ac.id>
2024-11-25 13:39:10 +0700

    backend-api

--- backend-api/app.js
+++ backend-api/app.js
@@ -7,22 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Initialize Firebase Admin
-const serviceAccount = {
-  // You'll need to fill this with your service account credentials
-  
-	"type": "service_account",
-	"project_id": "testing-442012",
-	"private_key_id": "6ec200738be1a21a419e6cb6ff1933c1ae2725a0",
-	"private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDsew9Wf5ta4vUG\nerEqfWPqSkeERiYYPJGcqLsIFLnGq4SEPJrudqlK49RU7b3YId3QMmEuucVBnYZy\nWjs58YbksYgqEd6ScVUkOWtJC6USwA3caTzTB+kkda7pue5cSAUPBrZVEuFXwig8\nBR+qTT8/4G44aVRASgUSAA3Afv1jz9mmuY/BQo4aIKP+p2/EsZd9Htztv4btHQlb\nBCfSVv7+wczySaD0nQbwSGv7ikaM5H/UfQhJbL6d7JPIv+ssJHSQvKu8EqvDI2Tu\n0cJTJvPMq4GK7hWVifDWTF1+2giJjkyBnaC4Buzrw/23ObTHe07lKQlap3vE+klg\nEqDZ2EvvAgMBAAECggEAUKUY0nE6/DAoyjheIbTYNYZ2RCQeCqbhK3EZM0TKI/fK\n/cOjPE5IJmtytjHLS6ElAUKlBKfCnsFHH8koqHZ2/5eRbXNhmUQ/+lYAgc0QUsgf\nEfrrjLYGanIdn8RSBrNi0kCCPpntMh3a1bkTluK3/qMdQ2ycGD6F4yEUj4BfDkGk\nPpR9Iot7KqcwwTRRuszw7dVTGUCE5eJAZziiK/yIKbqMVwfZyPqu9vC2q0cmwO7y\nHnrpYrOLZcMMh3AfMCLONs4tmzGXA7dclaU4cJ0rYvc1D/+687RCM7EFUn4eOIh2\nzzrUXKkLBImnYfPckSHIb+yp6LyTgzkQXiiWt/fdkQKBgQD9aiRbq4IDQincEtVx\nfgitXN26yR1+eO8NNdzwD+s4HZFc+ziYdhws9G/6UQoesiNmrr/PytP1WS6vgfEI\nrYIHz4LUoaP8Ea9q+Op/Ovqw1IjQN6Ix6PYgOJTkhaZeQI5lB0BTaKRKZh7xXtwj\n6ONzkiqQvA6X8UxWV4FeTFOgUQKBgQDu5LDJ9alLEdm0e/kWfrW/H4BSg53+Xo1R\nC6BseKEkJOHcA3U/8mNG4pBHsnyDSFMirC9L6gXNYgObBSlJgEc7fPhuUeXlQZLa\noIHiTQuNtqBzPX1ZmQ6D9Ub6kX0aQvd+JZmwQk4yu2GMYwy1h4TC73cz0GmdY+9t\nZ2kKDahYPwKBgB4wCbq92JQIyrHDsJd/ewsj1+oLUS8iut9o/jO+wbeCljFsX9aR\no1ObnsdD5h+3+s2dnY3kTJGamRwhL5RoDuhdSOmJg95gQPROSKj1Xq2388BSEEk/\nxioac2oxnZr2t7/DOF93RLL0LVV1hmXktUusKtPpAxgIj55teCTClOexAoGAZ6WN\nFnKhOtLJ08ad8aaUEBIIyMQV/lDjmzAiMC2+LEJcQ+q0KkRt0klmM9U6tHLirv5p\nntQwJEXMLqv0l9EyFptH5gVeazMiFfzhnV+0DY4Y2ybohcEkTRhT7U7GKmi3nGGP\n5qs95kDqN9CZqQDdFv5yh2Z7DDBZpuvg5N28Fl0CgYBmXh3rctn+P5jjUCx5KXZm\nyKXcmOZWbQT5y7zbE0cS6WrZmR8Xa2ynelVy/HUw9SL2TbQU/lxF9V0GzCcHAI9z\nYTij2QLYTlLzB9He4bfgpIMxPEx7Bmg8mEQ11PhXklRVZBKHdC2OglMSL8R46xDO\nV+MDs+tFQpu5J1HI2X8avw==\n-----END PRIVATE KEY-----\n",
-	"client_email": "firebase-adminsdk-lll6a@testing-442012.iam.gserviceaccount.com",
-	"client_id": "104875396769177569282",
-	"auth_uri": "https://accounts.google.com/o/oauth2/auth",
-	"token_uri": "https://oauth2.googleapis.com/token",
-	"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-	"client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-lll6a%40testing-442012.iam.gserviceaccount.com",
-	"universe_domain": "googleapis.com"
-};
+const serviceAccount = require('./key.json')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -37,14 +22,12 @@ app.get('/products', async (req, res) => {
       
       const products = [];
       
-      // Menggunakan forEach untuk mengiterasi setiap child (product_1, product_2, dst)
       snapshot.forEach((childSnapshot) => {
-        // childSnapshot.key akan mengambil product_1, product_2, dst
         const productId = childSnapshot.key;
         const productData = childSnapshot.val();
         
         products.push({
-          id: productId,  // Ini akan berisi "product_1", "product_2", dst
+          id: productId,  
           product_name: productData.product_name,
           categories: productData.categories
         });
@@ -57,10 +40,8 @@ app.get('/products', async (req, res) => {
     }
   });
   
-  // GET product detail by ID
   app.get('/products/:id', async (req, res) => {
     try {
-      // req.params.id akan berisi "product_1" atau "product_2" dst
       const productId = req.params.id;
       const ref = db.ref(`/${productId}`);
       
@@ -73,9 +54,8 @@ app.get('/products', async (req, res) => {
         });
       }
   
-      // Menambahkan ID ke response
       const response = {
-        id: productId,  // Ini akan berisi "product_1", "product_2", dst
+        id: productId,  
         ...productData
       };
   
