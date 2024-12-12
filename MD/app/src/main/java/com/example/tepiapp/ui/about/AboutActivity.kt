feced936f6ec650c30e3ae4594ca7c59753277a5
hoseajavier <112994063+hoseajavier@users.noreply.github.com>
2024-12-13 00:11:35 +0700

    Finalisasi MD

--- MD/app/src/main/java/com/example/tepiapp/ui/about/AboutActivity.kt
+++ MD/app/src/main/java/com/example/tepiapp/ui/about/AboutActivity.kt
@@ -2,6 +2,7 @@ package com.example.tepiapp.ui.about
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 import com.example.tepiapp.R
 
 class AboutActivity : AppCompatActivity() {
@@ -10,5 +11,14 @@ class AboutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
+        // Inisialisasi Toolbar
+        val topAppBar: MaterialToolbar = findViewById(R.id.topAppBar)
+        setSupportActionBar(topAppBar)
+
+        // Atur aksi tombol navigasi
+        topAppBar.setNavigationOnClickListener {
+            // Kembali ke activity sebelumnya
+            onBackPressed()
+        }
     }
 }
